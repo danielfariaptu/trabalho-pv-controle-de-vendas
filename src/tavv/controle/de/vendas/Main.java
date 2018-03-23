f805b298dd5b32088d865a19bd7b252a5f2c877d
Daniel Faria <danielfariaptu@gmail.com>
2018-03-23 05:09:25 -0300

    MAIN QUE CHAMA LOGIN

---
+++ src/tavv/controle/de/vendas/Main.java
@@ -0,0 +1,25 @@
+package tavv.controle.de.vendas;
+
+import javax.swing.JFrame;
+
+
+
+public class Main { 
+
+ public static void main(String[] args) {
+         Login lg = new Login ();
+              	lg.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		lg.setSize(600, 550);
+                lg.setLocationRelativeTo(null);
+		lg.setVisible(true);
+ 
+    }
+ 
+
+  
+  
+ 
+
+} 
+
+
